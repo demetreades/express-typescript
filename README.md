54f738d23dddbb0c523868683d468b192f57b869
demet <demet@softweb.gr>
2022-03-10 19:03:35 +0200

    git init project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+npx gitignore node
+
+npm i express module-alias && npm i -D dotenv typescript tsc-watch eslint prettier eslint-config-prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-prettier @types/node @types/express
+
+npm i mongoose compression cors morgan helmet
+
+npx tsc --init
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import Controller from '@/utils/interfaces/controller.interface';
+import ErrorMiddleware from '@/middleware/error.middleware';
+
+class App {
+  public express: Application;
+  public port: number;
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+
+    this.initialiseDatabaseConnection();
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+  }
+}
